7ecce3ea9247f779de58c5516470221daa0299b7
y254 <1459090770@qq.com>
2021-03-29 17:07:52 +0800

    配置了项目中的vant

--- babel.config.js
+++ babel.config.js
@@ -2,4 +2,12 @@ module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ]
+  // 这是vant提供的自动按需加载
+  // plugins: [
+  //   ['import', {
+  //     libraryName: 'vant',
+  //     libraryDirectory: 'es',
+  //     style: true
+  //   }, 'vant']
+  // ]
 }
--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 导入vant框架和样式
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+// 全局的把vant所有的组件都导入了
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
